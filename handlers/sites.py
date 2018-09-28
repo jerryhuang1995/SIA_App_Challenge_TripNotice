1d217b86c618865be6f574a1a4630f64cf9d8a24
Chiahao Lin <cl@tripnotice.com>
2018-09-28 15:33:35 +0800

    chore: index.html

--- handlers/sites.py
+++
@@ -1,8 +0,0 @@
-import tornado.web
-import methods.readcsv as mrc
-
-class SitesHandler(tornado.web.RequestHandler):
-    def get(self):
-        dest = self.get_argument("dest")
-        dest_rels = mrc.read_csv(dest)
-        self.render("sites.html", rels = dest_rels)
