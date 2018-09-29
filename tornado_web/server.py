ec8b5d14bf20b1d05e415da0c11cda99631ab571
Tsun-Han Huang <jerry19950709@gmail.com>
2018-09-30 01:02:26 +0800

    fix(data): no need to define SiteNode class

--- tornado_web/server.py
+++ tornado_web/server.py
@@ -4,33 +4,10 @@
 import tornado.ioloop
 import tornado.options
 import tornado.httpserver
-
 from application import application
-
 from tornado.options import define, options
 
 define("port", default = 8000, help = "run on the given port", type = int)
-
-class siteNode(object):
-    node_list = []
-
-    def __init__(self, name, rank):
-        self.name = name
-        self.rank = rank
-
-    def recommend(self):
-        sort_dict = {}
-
-        for i, site in enumerate(self.node_list):
-            sort_dict[site.name] = {}
-            sort_dict[site.name]['relate'] = i+1
-
-        ranking = sorted(self.node_list, key=lambda x: x.rank)
-
-        for i, site in enumerate(ranking):
-            sort_dict[site.name]['rank'] = i+1
-
-        return sort_dict
 
 def main():
     tornado.options.parse_command_line()
